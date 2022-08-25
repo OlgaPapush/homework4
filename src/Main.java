b0dbf13c58935603cb080310a5257092f16ab453
Ольга <koval.oi.1106@gmail.com>
2022-08-25 10:44:29 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,27 @@ public class Main {
         } else {
             System.out.println(" Установите версию на Android ");
         }
+
+
+        // задание 2
+        int ClientOs = 0;
+        int clientDeviseYear = 2022;
+                switch ( clientOs ) {
+            case 1:
+                if ( clientOs == 0 && clientDeviseYear >= 2015 ) {
+                    System.out.println("Установите  версию на Ios по ссылке");
+                }
+                break;
+            case 2:
+                if ( clientOs == 1 && clientDeviseYear >= 2015 ) {
+                    System.out.println("Установите  версию на Android по ссылке"); }
+                    break;
+                    default:
+                        System.out.println ( " Установите  облегченную версию по ссылке") ;
+
+
+
+                }
+        }
+
     }
-}
