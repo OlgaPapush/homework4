4ac72f6de889d626c8048d55e6de94fbca1ddd43
Ольга <koval.oi.1106@gmail.com>
2022-08-25 09:35:21 +0300

    1 задача

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        // задание 1
+        int clientOs;
+        clientOs = 0;
+        if (clientOs == 0) {
+            System.out.println(" Установите версию на Ios");
+        } else {
+            System.out.println(" Установите версию на Android ");
+        }
+    }
+}
